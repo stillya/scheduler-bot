c395c4de1cc78eb27195642fddcaef7e1244be75
Ilya Starchenko <stillya@MacBook.local>
2020-09-08 01:57:40 +0600

    INITIAL COMMIT

---
+++ src/test/java/org/omstu/bot/sheduler/ShedulerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.omstu.bot.sheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShedulerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
