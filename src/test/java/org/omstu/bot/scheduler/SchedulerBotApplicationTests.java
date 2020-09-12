2c3c3beb5dcc086737765e2024e9bef795e31fbd
stillya <redux12345678@mail.ru>
2020-09-13 00:11:08 +0600

    small fix;

--- src/test/java/org/omstu/bot/scheduler/SchedulerBotApplicationTests.java
+++ src/test/java/org/omstu/bot/scheduler/SchedulerBotApplicationTests.java
@@ -1,10 +1,10 @@
-package org.omstu.bot.sheduler;
+package org.omstu.bot.scheduler;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShedulerBotApplicationTests {
+class SchedulerBotApplicationTests {
 
 	@Test
 	void contextLoads() {
