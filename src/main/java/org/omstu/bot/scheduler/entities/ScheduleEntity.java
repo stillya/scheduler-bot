eb3026bded1086e248a632b1df51e5288ef12b68
Ilya Starchenko <stillya@MacBook.local>
2020-09-12 23:31:10 +0600

    Создал структуру для получения расписания.

--- src/main/java/org/omstu/bot/scheduler/entities/ScheduleEntity.java
+++ src/main/java/org/omstu/bot/scheduler/entities/ScheduleEntity.java
@@ -3,16 +3,73 @@ package org.omstu.bot.scheduler.entities;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Builder;
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@Data
-@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ScheduleEntity {
 
+    private String beginLesson;
+    private String dayOfWeekString;
+    private String discipline;
+    private String endLesson;
+    private String kindOfWork;
+    private String lecturer;
     private String auditorium;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "hh:mm")
-    private Date beginLesson;
-    private Date date;
+
+    public String getBeginLesson() {
+        return beginLesson;
+    }
+
+    public void setBeginLesson(String beginLesson) {
+        this.beginLesson = beginLesson;
+    }
+    
+    public String getAuditorium() {
+        return auditorium;
+    }
+
+    public void setAuditorium(String auditorium) {
+        this.auditorium = auditorium;
+    }
+
+    public String getDayOfWeekString() {
+        return dayOfWeekString;
+    }
+
+    public void setDayOfWeekString(String dayOfWeekString) {
+        this.dayOfWeekString = dayOfWeekString;
+    }
+
+    public String getDiscipline() {
+        return discipline;
+    }
+
+    public void setDiscipline(String discipline) {
+        this.discipline = discipline;
+    }
+
+    public String getEndLesson() {
+        return endLesson;
+    }
+
+    public void setEndLesson(String endLesson) {
+        this.endLesson = endLesson;
+    }
+
+    public String getKindOfWork() {
+        return kindOfWork;
+    }
+
+    public void setKindOfWork(String kindOfWork) {
+        this.kindOfWork = kindOfWork;
+    }
+
+    public String getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(String lecturer) {
+        this.lecturer = lecturer;
+    }
 
 }
