1f89efb41496f62787804da96c59edd3c766ffe1
Ilya Starchenko <stillya@MacBook.local>
2020-09-12 23:36:25 +0600

    small fix

--- src/main/java/org/omstu/bot/scheduler/controllers/SchedulerBotController.java
+++ src/main/java/org/omstu/bot/scheduler/controllers/SchedulerBotController.java
@@ -1,10 +1,7 @@
 package org.omstu.bot.scheduler.controllers;
 
 import lombok.AllArgsConstructor;
-import org.omstu.bot.scheduler.entities.ScheduleEntity;
 import org.omstu.bot.scheduler.services.bot.SchedulerTelegramBot;
-import org.omstu.bot.scheduler.services.parser.ScheduleParser;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,18 +13,9 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public class SchedulerBotController {
 
     SchedulerTelegramBot schedulerTelegramBot;
-    ScheduleParser parser;
 
-    @GetMapping(path = "/webhook", produces = "application/json;charset=UTF-8")
-    public ScheduleEntity[] onUpdateReceived() {
-        return this.parser.parse();
-    }
-}
-
-/*
-@PostMapping(path = "/webhook")
+    @PostMapping(path = "/webhook")
     public BotApiMethod onUpdateReceived(@RequestBody Update update) {
         return this.schedulerTelegramBot.onWebhookUpdateReceived(update);
     }
-    ?
- */
+}
--- src/main/java/org/omstu/bot/scheduler/services/bot/SchedulerTelegramBot.java
+++ src/main/java/org/omstu/bot/scheduler/services/bot/SchedulerTelegramBot.java
@@ -6,28 +6,28 @@ import org.omstu.bot.scheduler.services.bot.implementation.HandlerDecider;
 import org.omstu.bot.scheduler.services.bot.intefaces.UpdateHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class SchedulerTelegramBot extends TelegramWebhookBot {
 
-    @Autowired
     private HandlerDecider handlerDecider;
 
     @Value("${schedulerBot.name}")
     private String botUsername;
     @Value("${schedulerBot.token}")
-    String botToken;
+    private String botToken;
     @Value("${schedule.OMSTU}")
-    String botPath;
+    private String botPath;
 
-    public SchedulerTelegramBot(DefaultBotOptions defaultBotOptions) {
+    public SchedulerTelegramBot(DefaultBotOptions defaultBotOptions, HandlerDecider handlerDecider) {
         super(defaultBotOptions);
+        this.handlerDecider = handlerDecider;
     }
 
     @Override
