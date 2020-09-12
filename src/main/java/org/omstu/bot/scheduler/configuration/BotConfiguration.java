ae806eae6ac96432c76619d480449de403cdbc1f
Ilya Starchenko <stillya@MacBook.local>
2020-09-12 23:33:07 +0600

    some fixes

--- src/main/java/org/omstu/bot/scheduler/configuration/BotConfiguration.java
+++ src/main/java/org/omstu/bot/scheduler/configuration/BotConfiguration.java
@@ -1,20 +1,31 @@
 package org.omstu.bot.scheduler.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.omstu.bot.scheduler.services.bot.SchedulerTelegramBot;
+import org.omstu.bot.scheduler.services.bot.implementation.HandlerDecider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.client.RestTemplate;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.ApiContext;
 
 @Configuration
 public class BotConfiguration {
 
     @Bean
-    ObjectMapper objectMapper() {
+    public ObjectMapper objectMapper() {
         return new ObjectMapper();
     }
 
     @Bean
-    RestTemplate restTemplate() {
+    public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public SchedulerTelegramBot schedulerTelegramBot(HandlerDecider handlerDecider) {
+        DefaultBotOptions options = ApiContext.getInstance(DefaultBotOptions.class);
+        return new SchedulerTelegramBot(options, handlerDecider);
     }
 }
--- src/main/java/org/omstu/bot/scheduler/controllers/SchedulerBotController.java
+++ src/main/java/org/omstu/bot/scheduler/controllers/SchedulerBotController.java
@@ -1,7 +1,10 @@
 package org.omstu.bot.scheduler.controllers;
 
 import lombok.AllArgsConstructor;
+import org.omstu.bot.scheduler.entities.ScheduleEntity;
 import org.omstu.bot.scheduler.services.bot.SchedulerTelegramBot;
+import org.omstu.bot.scheduler.services.parser.ScheduleParser;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +16,18 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public class SchedulerBotController {
 
     SchedulerTelegramBot schedulerTelegramBot;
+    ScheduleParser parser;
 
-    @PostMapping(path = "/webhook")
+    @GetMapping(path = "/webhook", produces = "application/json;charset=UTF-8")
+    public ScheduleEntity[] onUpdateReceived() {
+        return this.parser.parse();
+    }
+}
+
+/*
+@PostMapping(path = "/webhook")
     public BotApiMethod onUpdateReceived(@RequestBody Update update) {
         return this.schedulerTelegramBot.onWebhookUpdateReceived(update);
     }
-}
+    ?
+ */
